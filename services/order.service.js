cdbe3cbb90526efd9135e9b618e642a88c1cedba
Alejandro Chavez <alejandrochavez1202@gmail.com>
2023-03-23 23:13:45 -0400

    Added JWT in each endpoint

--- services/order.service.js
+++ services/order.service.js
@@ -3,15 +3,21 @@ const boom = require('@hapi/boom');
 const { models } = require('./../libs/sequelize');
 
 class OrderService {
-
-  constructor(){
-  }
+  constructor() {}
 
   async create(data) {
-    const newOrder = await models.Order.create(data);
+    const customer = await models.Customer.findOne({
+      where: {
+        '$user.id$': data.userId,
+      },
+      include: ['user'],
+    });
+    if (!customer) {
+      throw boom.badRequest('Customer not found');
+    }
+    const newOrder = await models.Order.create({ customerId: customer.id });
     return newOrder;
   }
-
   async addItem(data) {
     const newItem = await models.OrderProduct.create(data);
     return newItem;
@@ -21,15 +27,31 @@ class OrderService {
     return [];
   }
 
+  async findByUser(userId) {
+    const orders = await models.Order.findAll({
+      where: {
+        '$customer.user.id$': userId,
+      },
+      include: [
+        {
+          association: 'customer',
+          include: ['user'],
+        },
+        'items',
+      ],
+    });
+    return orders;
+  }
+
   async findOne(id) {
     const order = await models.Order.findByPk(id, {
       include: [
         {
           association: 'customer',
-          include: ['user']
+          include: ['user'],
         },
-        'items'
-      ]
+        'items',
+      ],
     });
     return order;
   }
@@ -44,7 +66,6 @@ class OrderService {
   async delete(id) {
     return { id };
   }
-
 }
 
 module.exports = OrderService;
